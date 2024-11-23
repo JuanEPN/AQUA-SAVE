8ae744e8f5c6723a4cbe41cdc0937efbd1b8d7f0
JuanEPN <juan.pereira.neira@correounivalle.edu.co>
2024-11-23 13:20:02 -0500

    A

--- src/pages/quiz/Quiz.jsx
+++ src/pages/quiz/Quiz.jsx
@@ -1,3 +1,12 @@
-import "./Quiz.css";
+import React from 'react';
+import './Quiz.css';
+
+const Quiz = () => {
+  return (
+    <div>
+      <h1>Quiz Component</h1>
+    </div>
+  );
+};
 
 export default Quiz;
